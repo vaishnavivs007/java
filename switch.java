0bc4d04755176883bb3c37fd4441c0c6a0f492b2
vaishnavivs007 <vaishnavishejwadkar@gmail.com>
2025-04-04 08:33:33 +0530

    save the java file

---
+++ switch.java
@@ -0,0 +1,18 @@
+public class switch{
+    public static void main(string[] args){
+        int month = 5;
+        switch(month){
+            case 1:System.out.println("Jan");
+            break;
+            case 2:System.out.println("Feb");
+            break;
+            case 3:System.out.println("Mar");
+            break;
+            case 4:System.out.println("Apr");
+            break;
+            case 5:System.out.println("May");
+            break;
+            default:System.out.println("Invalid Month");
+        }
+    }
+}
